a8679c2b4addc172ff0d3d11eb01ec37562276b6
Rowan James <rowanj@buggyco.de>
2019-12-21 23:52:33 +1100

    use a lockfile to serialise requests to run r10k

--- run-r10k.sh
+++ run-r10k.sh
@@ -5,4 +5,5 @@ R10K_ARGS=${R10K_ARGS:-deploy environment -v -p}
 echo "Running r10k ${R10K_ARGS}"
 
 # webhook forwards to r10k's docker-entrypoint.sh
-exec /docker-entrypoint.sh $R10K_ARGS
+# use a lockfile to serialise requests
+exec flock --timeout 55 run-r10k.lock /docker-entrypoint.sh $R10K_ARGS
